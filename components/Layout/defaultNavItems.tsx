d3adde834174f1dad8e6666deef9655907e647ed
SheetalMetiz <129273434+SheetalMetiz@users.noreply.github.com>
2023-04-04 19:29:22 +0530

    Add files via upload

---
+++ components/Layout/defaultNavItems.tsx
@@ -0,0 +1,30 @@
+import { NavItem } from "./Sidebar";
+
+// Sidebar Menu
+export const defaultNavItems: NavItem[] = [
+  {
+    label: "GENESIS PASS",
+    href: "/",
+    icon: "",
+  },
+  {
+    label: "SERVICES",
+    href: "/Services",
+    icon: "",
+  },
+  {
+    label: "PROJECTS",
+    href: "/Projects",
+    icon: "",
+  },
+  {
+    label: "NFT",
+    href: "/Nft",
+    icon: "",
+  },
+  {
+    label: "FAQ",
+    href: "/Faq",
+    icon: "",
+  },
+]
